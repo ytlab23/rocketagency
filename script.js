8b30a0da2e9a9c6964eb3af538c36513c89a48c7
Kingsley Awoke <kingsley_business@proton.me>
2024-04-03 08:56:30 +0100

    Finished the header part.

---
+++ script.js
@@ -0,0 +1,43 @@
+'use stric';
+
+// || EVENT ON ELEMENT || //
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+
+// || TOGGLE NAVBAR || //
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+    navbar.classList.remove("active");
+    navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+// || HEADER || //
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove("active");
+    }
+})
