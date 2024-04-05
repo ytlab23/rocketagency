6183705b93bce23e187dc4a4df39e19500a1526e
Kingsley Awoke <kingsley_business@proton.me>
2024-04-05 17:18:41 +0100

    Built Up to MVP

--- script.js
+++ script.js
@@ -33,11 +33,19 @@ addEventOnElem(navbarLinks, "click", closeNavbar);
 
 // || HEADER || //
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector('[data-back-top-btn]');
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         header.classList.add('active');
+        backTopBtn.classList.add('active');
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 })
+
+// || ALERT || \\
+function showAlert() {
+    alert("Thank you for submitting!");
+}
